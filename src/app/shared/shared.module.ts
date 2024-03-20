d8ee5181f5c714c57b7bd6e92c1cb1fd333d565d
fabian <fabian@hotmail.com>
2024-03-20 00:50:15 -0500

    finalizo ruta básica

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+
+import { RouterModule } from '@angular/router';
+
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [HomePageComponent,AboutPageComponent, ContactPageComponent, SidebarComponent]
+})
+export class SharedModule { }
